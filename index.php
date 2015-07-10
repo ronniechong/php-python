3081a8d4022c2bbfff5195304959c699abe9596b
Ronnie Chong <ronnie@otakuness.com>
2015-07-10 23:56:06 +1000

    Update markup

--- index.php
+++ index.php
@@ -1,26 +1,29 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Pi Generator</title>
+    <title>Pi Generator with Python</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="main.css">
   </head>
   <body>
     <div class="container">
-      <h1>Generator!</h1>
-      <form id="form">
-        <div class="form__container">
-          <label for="form__input--number">Enter decimals</label>
-          <input type="number" id="form__input--number"/>
-        </div>
-        <div class="form__container">
-          <input type="submit" value="Generate"/>
-        </div>
-        <span class="form__loader"></span>
-        <span id="time"></span>
-      </form>
-      <div id="output"></div>
+      <div class="box">
+        <h1>Generator!</h1>
+        <form id="form">
+          <div class="form__container">
+            <label for="form__input--number">Enter decimals</label>
+            <input type="number" id="form__input--number"/>
+          </div>
+          <div class="form__container">
+            <input type="submit" value="Generate"/>
+          </div>
+          <span class="form__loader"></span>
+          <span id="time"></span>
+        </form>
+        <div id="output"></div>
+      </div>
+      <small><a href="https://github.com/ronniechong/php-python" target="_blank" title="php-python at Github">https://github.com/ronniechong/php-python</a></small>
     </div>
     <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
     <script type="text/javascript" src="main.js"></script>
