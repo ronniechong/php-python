81b7992a390863ada13f02f08cdc80bb59f42ea3
Ronnie Chong <ronnie@otakuness.com>
2015-07-10 23:11:34 +1000

    no need

--- lib/lib.php
+++ lib/lib.php
@@ -1,7 +1,7 @@
 <?php
     if ($_POST){
         $num            = $_POST["num"];
-        $output         = `python pi.py $num`;
+        $output         = `pi.py $num`;
         echo $output;
     }
 ?>
